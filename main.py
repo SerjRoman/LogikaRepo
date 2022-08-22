3229900e6f891058caa931026a11a703e5555529
Felix <felixdnbox@gmail.com>
2022-08-22 20:01:29 +0300

    я тут был

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import turtle
 
 main_turtle = turtle.Turtle()
 
-
-
-
+def draw_circle():
+    main_turtle.penup()
+    main_turtle.goto(0,0)
+    main_turtle.pendown()
+    main_turtle.color('red')
+    main_turtle.begin_fill()
+    main_turtle.circle(100)
+    main_turtle.end_fill()
+draw_circle()
 
 main_turtle.hideturtle()
 turtle.exitonclick()
