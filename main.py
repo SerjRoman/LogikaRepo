2bda94e1457ae850d42c338b85cd31bcb95c875c
GeniyOfPython <anton.primush123663@gmail.com>
2022-08-22 19:58:07 +0300

    Tosha purple figure

--- main.py
+++ main.py
@@ -2,7 +2,24 @@ import turtle
 
 main_turtle = turtle.Turtle()
 
-
+def purple_figure(x, y):
+    main_turtle.color('purple')
+    main_turtle.width(5)
+    
+    main_turtle.penup()
+    main_turtle.goto(x,y)
+    main_turtle.pendown()
+    
+    main_turtle.forward(50)
+    main_turtle.left(70)
+    main_turtle.forward(50)
+    main_turtle.forward(25)
+    main_turtle.left(20)
+    main_turtle.left(90)
+    main_turtle.forward(50)
+    main_turtle.left(70)
+    main_turtle.forward(50)
+    main_turtle.forward(25)
 
 
 
