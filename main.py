af74f7beba9f63f81ad624f3bdae420fd882f625
Taisiia <kiprictaisia2@gmail.com>
2022-08-22 18:41:45 +0200

    123

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import turtle
 
 main_turtle = turtle.Turtle()
 
+def Turtle():
+    main_turtle.pendown()
+    main_turtle.left(45)
+    main_turtle.width(6)
+    for el in range(4):
+        main_turtle.forward(100)
+        main_turtle.left(90)
 
 
-
-
+Turtle()
 main_turtle.hideturtle()
 turtle.exitonclick()
