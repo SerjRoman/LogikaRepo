1bf56d89570befa9ed63c6415655989d30df9fc3
Yarik <krosskalambus@gmail.com>
2022-08-22 19:21:13 +0200

    mne nadoelo zhdat

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
+
 import turtle
 
 main_turtle = turtle.Turtle()
+def YarikTelius(x,y):
+    main_turtle.width(7)
+    
+    main_turtle.color("yellow")
+    main_turtle.penup()
+    main_turtle.goto(x,y)
+    main_turtle.pendown()
+    main_turtle.forward(50)
+    main_turtle.left(105)
+    main_turtle.forward(35)
+    main_turtle.left(75)
+    main_turtle.forward(35)
+    main_turtle.left(75)
+    main_turtle.forward(35)
 
 
-
-
+YarikTelius(50,50)
 
 main_turtle.hideturtle()
 turtle.exitonclick()
