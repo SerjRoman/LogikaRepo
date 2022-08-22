a6ab2ed7b4ec3b9237ccb736e7d4980847b00138
Yaroslav Samchuk <samchukyaroslav@gmail.com>
2022-08-22 19:50:31 +0300

    Yaroooororo_Nagibator_hAhHA228000_2023

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ import turtle
 
 main_turtle = turtle.Turtle()
 
+def rarka():
+    main_turtle.shapesize(3)
+    main_turtle.color("green")
+    main_turtle.pendown()
+    main_turtle.forward(100)
+    main_turtle.right(180)
+    main_turtle.right(50)
+    main_turtle.forward(70)
+    main_turtle.left(95)
+    main_turtle.forward(80)
 
 
-
+rarka()
 
 main_turtle.hideturtle()
 turtle.exitonclick()
