05d3dac122193c9f5ca775a444ad6d8efe71af4a
Vlad <ledovski2007@gmai.com>
2022-08-22 19:47:26 +0300

    Vald Pentagon

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ import turtle
 
 main_turtle = turtle.Turtle()
 
+def Pentagon(x, y):
 
+    main_turtle.color("green")
 
+    main_turtle.penup()
+    main_turtle.goto(x, y)
+    main_turtle.pendown()
 
+    for i in range(5):
+        main_turtle.forward(100)
+        main_turtle.left(72)
+    
+Pentagon(-200, -200)
 
 main_turtle.hideturtle()
 turtle.exitonclick()
