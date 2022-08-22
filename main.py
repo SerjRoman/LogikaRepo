5b16d86f1d004a9eeb2aa2816bf43f7fb3c214c2
Serj <sergeyroman26@gmail.com>
2022-08-22 18:10:04 +0100

    Fixed TriAngle

--- main.py
+++ main.py
@@ -2,21 +2,20 @@ import turtle
 
 main_turtle = turtle.Turtle()
 
-def rarka():
+def rarka(x,y):
     main_turtle.penup()
-    main_turtle.goto(100,100)
+    main_turtle.goto(x,y)
     main_turtle.shapesize(3)
     main_turtle.color("green")
     main_turtle.pendown()
-    main_turtle.forward(100)
-    main_turtle.right(180)
-    main_turtle.right(50)
-    main_turtle.forward(70)
-    main_turtle.left(95)
-    main_turtle.forward(80)
+    main_turtle.forward(50)
+    main_turtle.left(120)
+    main_turtle.forward(50)
+    main_turtle.left(120)
+    main_turtle.forward(50)
 
 
-rarka()
+rarka(100,100)
 
 main_turtle.hideturtle()
 turtle.exitonclick()
