101c320f580b6f01b785c49fc4dca477c671ecaf
Serj <sergeyroman26@gmail.com>
2022-08-22 18:06:58 +0100

    Fixed Circle

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import turtle
 
 main_turtle = turtle.Turtle()
 
-def draw_circle():
+def draw_circle(x,y):
     main_turtle.penup()
-    main_turtle.goto(0,0)
+    main_turtle.goto(x,y)
     main_turtle.pendown()
     main_turtle.color('red')
     main_turtle.begin_fill()
-    main_turtle.circle(100)
+    main_turtle.circle(50)
     main_turtle.end_fill()
-draw_circle()
+draw_circle(100,100)
 
 main_turtle.hideturtle()
 turtle.exitonclick()
