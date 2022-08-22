8eecf4408b07f33d4f5ded5feb7e181900138ba8
Serj <sergeyroman26@gmail.com>
2022-08-22 18:16:16 +0100

    Fixed Ilya

--- main.py
+++ main.py
@@ -2,21 +2,20 @@ import turtle
 
 main_turtle = turtle.Turtle()
 
-def ilya():
-    main_turtle.goto(200, 200)
-    main_turtle.begin_fill()
-    main_turtle.color(0, 0, 0)
-    main_turtle.fillcolor(0, 0, 0)
+def ilya(x,y):
+    main_turtle.penup()
+    main_turtle.goto(x, y)
     main_turtle.pendown()
-    main_turtle.left(45)
+    main_turtle.begin_fill()
+    main_turtle.fillcolor('black')
     main_turtle.forward(50)
-    main_turtle.left(225)
-    main_turtle.forward(50)
-    main_turtle.left(90)
+    main_turtle.left(135)
+    main_turtle.forward(70)
+    main_turtle.left(135)
     main_turtle.forward(50)
     main_turtle.end_fill()
-    main_turtle.penup()
-
+    
+ilya(100,100)
 
 
 
