8da9a0457f819858853a6ec0f9a6989036e2c4df
Serj <sergeyroman26@gmail.com>
2022-08-22 18:18:15 +0100

    Fixed

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import turtle
 
 main_turtle = turtle.Turtle()
-def figura():
+def figura(x,y):
     main_turtle.color("blue")
     main_turtle.penup()
-    main_turtle.goto(-50,0)
+    main_turtle.goto(x,y)
     main_turtle.pendown()
     for i in range(6):
-     main_turtle.forward(100)
-     main_turtle.left(60)
-figura()
+        main_turtle.forward(100)
+        main_turtle.left(60)
+figura(100,100)
 main_turtle.hideturtle()
 turtle.exitonclick()
