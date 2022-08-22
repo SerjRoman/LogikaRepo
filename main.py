a79446c696c00666fedec31d5b37f2d7a0619ae0
nikita <nikitos09102008@gmail.com>
2022-08-22 19:49:04 +0300

    figura nikiti uluchena

--- main.py
+++ main.py
@@ -3,18 +3,9 @@ import turtle
 main_turtle = turtle.Turtle()
 def figura():
     main_turtle.color("blue")
-    main_turtle.forward(100)
-    main_turtle.left(60)
-    main_turtle.forward(100)
-    main_turtle.left(60)
-    main_turtle.forward(100)
-    main_turtle.left(60)
-    main_turtle.forward(100)
-    main_turtle.left(60)
-    main_turtle.forward(100)
-    main_turtle.left(60)
-    main_turtle.forward(100)
-    main_turtle.left(60)
+    for i in range(6):
+     main_turtle.forward(100)
+     main_turtle.left(60)
     
 
 
