007f9633d0cabba01ac8a8885b2dbdef6d5d25e1
nikita <nikitos09102008@gmail.com>
2022-08-22 19:46:24 +0300

    figura nikiti

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import turtle
 
 main_turtle = turtle.Turtle()
+def figura():
+    main_turtle.color("blue")
+    main_turtle.forward(100)
+    main_turtle.left(60)
+    main_turtle.forward(100)
+    main_turtle.left(60)
+    main_turtle.forward(100)
+    main_turtle.left(60)
+    main_turtle.forward(100)
+    main_turtle.left(60)
+    main_turtle.forward(100)
+    main_turtle.left(60)
+    main_turtle.forward(100)
+    main_turtle.left(60)
+    
 
 
 
 
 
+figura()
 main_turtle.hideturtle()
 turtle.exitonclick()
